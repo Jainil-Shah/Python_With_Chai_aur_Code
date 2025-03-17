62630a4103b05b43093c2aed6aebe5e3606ce4b4
Jainil-Shah <jainilshah26222@gmail.com>
2025-03-17 11:48:46 +0530

    Second Commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-pass
+def even_generator(limit):
+    for i in range(2, limit+1, 2):
+        yield i
+        
+for num in even_generator(10):
+    print(num)
+
+gen = even_generator(10)
+
+print("-------------------")
+print(next(gen))
+print(next(gen))
+
